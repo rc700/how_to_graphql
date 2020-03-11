ce141080218cc493746d4fa05363f6df1e274d96
Rachael <rachael.coulter@kairostech.io>
2020-03-11 10:24:09 +0000

    Setting up Graphiql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,7 @@
 module Types
-  # All GraphQL queries start from a root type called Query
-  class QueryType < Types::BaseObject
-
-    # Links 
+  class QueryType < BaseObject
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
     field :all_links, [LinkType], null: false
 
     # this method is invoked, when `all_link` fields is being resolved
